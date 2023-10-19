10978850fbe90c53d7adf641491c4eb14061a12e
shiruiz2208 <gise.13.ruiz@gmail.com>
2023-10-19 02:27:19 -0300

    ejercicios tp 1

--- js/2-variablesCondicionales.js
+++ js/2-variablesCondicionales.js
@@ -1,5 +1,38 @@
 //Ejercicio 1: Variables y Operaciones Matemáticas //
-let comision = "74i";
+let number1 = 4;
+let numer2 = 2;
 
-console.log = (comision);
+console.log( number1 + numer2);
+
+console.log(number1 - numer2);
+
+console.log (number1 *numer2);
+
+console.log (number1 / numer2);
+
+//Ejercicio 2: Concatenación de Cadenas//
+
+let nombre = "Juan";
+let apellido = "Perez";
+let nombreCompleto = "Juan Perez";
+
+console.log( nombre);
+console.log (apellido);
+console.log(nombreCompleto);
+
+//Ejercicio 3: Conversión de Grados Celsius a Fahrenheit//
+//// Declarar la variable gradosCelsius y asignarle un valor en grados Celsius
+var gradosCelsius = 25;
+
+// Aplicar la fórmula de conversión para calcular los grados Fahrenheit
+var gradosFahrenheit = (gradosCelsius * 9/5) + 32;
+
+// Mostrar ambos valores en la consola
+console.log("Grados Celsius: " + gradosCelsius);
+console.log("Grados Fahrenheit: " + gradosFahrenheit);
+l
+
+
+
+
 
