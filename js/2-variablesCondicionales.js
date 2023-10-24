5ab27d89b8c10f93905f7d374b7ab2134a98d190
shiruiz2208 <gise.13.ruiz@gmail.com>
2023-10-23 21:39:55 -0300

    Agregue conceptos basicos js

---
+++ js/2-variablesCondicionales.js
@@ -0,0 +1,56 @@
+//Esto es un comentario de una sola linea //
+
+/*Esto es 
+un comentario 
+de 
+multples lineas*/
+
+
+//Conceptos Basicos-inicializar variables//
+let comision = "c74i";
+
+//Mostrar mensaje //
+console.log ( 7 + 8);
+console.log(comision);
+console.log("comision" + comision);
+
+
+//TP NUM 1 : Ejercicio 1: Variables y Operaciones Matemáticas //
+let number1 = 4;
+let numer2 = 2;
+
+console.log( number1 + numer2);
+
+console.log(number1 - numer2);
+
+console.log (number1 *numer2);
+
+console.log (number1 / numer2);
+
+//Ejercicio 2: Concatenación de Cadenas//
+
+let nombre = "Juan";
+let apellido = "Perez";
+let nombreCompleto = "Juan Perez";
+
+console.log( nombre);
+console.log (apellido);
+console.log(nombreCompleto);
+
+
+//Ejercicio 3: Conversión de Grados Celsius a Fahrenheit//
+//// Declarar la variable gradosCelsius y asignarle un valor en grados Celsius
+var gradosCelsius = 25;
+
+
+var gradosFahrenheit = (gradosCelsius * 9/5) + 32;
+
+
+console.log("Grados Celsius: " + gradosCelsius);
+console.log("Grados Fahrenheit: " + gradosFahrenheit);
+l
+
+
+
+
+
