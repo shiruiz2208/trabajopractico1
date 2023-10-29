1a45b7c3d8cad039c40cf72725ea4eabab72aec1
shiruiz2208 <gise.13.ruiz@gmail.com>
2023-10-29 17:28:05 -0300

    agregamos prompt

--- ejercicioscampus.js
+++ ejercicioscampus.js
@@ -4,27 +4,37 @@ document.write("**ejercicio 1");alert("un mensaje"); */
 
 alert ("Hello world");
 
-//ejercicio 2
+//ejercicio 2 - Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write).
+
 
 document.write("Hello world");
 
-//ejercicio 3
+//ejercicio 3: Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
 
 document.write("<br>3+5="+(3+5));
 
-//ejercicio 4
+//ejercicio 4  Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»,Ejemplo: 
+//input: Coder// 
+//Output: Hola Coder//
 
 
 let nombre = prompt("ingrese su nombre");
 document.write("<br>hola "+ nombre);
 
-//ejercicio 5
+//ejercicio 5 5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+//Nota: Tener en cuenta la siguiente función: parseInt
+//https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/parseInt//
+
 
 let num1 =  parseInt(prompt("ejercicio 5/ingrese numero"));
 let num2 =  parseInt(prompt("ejercicio 5/ingrese otro numero"));
 document.write("<br>resultado: "+(num1+num2));
 
-//ejercicio 6
+//ejercicio 6 Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+//Ejemplo: 
+//input: 15 , 3//
+//Output: El 15 es el número más grande//
+
 
 let num3 =  parseInt(prompt("ejercicio 6/ingrese numero"));
 let num4 =  parseInt(prompt("ejercicio 6/ingrese otro numero"));
@@ -35,7 +45,10 @@ if(num3>num4){
     document.write("<br>el mayor es: "+(num4));
 }
 
-//ejercicio 7
+//ejercicio 7 Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
+//Ejemplo:// 
+//input: 15 , 3, 9//
+//Output: El 15 es el número más grande//
 
 document.write("<br><br>**ejercicio 7<br>");
 
@@ -55,7 +68,14 @@ if(num7>=num5 && num7>=num6){
     document.write("<br>el mayor es: "+(num7));
 }
 
-//ejercicio 8
+//*ejercicio 8 : 8.- Escribe un programa que pida un número y diga si es divisible por 2
+//Ejemplo: 
+
+//input: 10
+//input: 15
+//Output: El 10 es divisible por 2.
+//Output: El 15 no es divisible por 2.//
+
 
 document.write("<br><br>**ejercicio 8<br>");
 
@@ -67,3 +87,28 @@ if(num8%2===0){
     document.write("<br>"+num8+" no es divisible por 2");
 }
 
+/*ejercicio 9 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+//Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+//Ejemplo://
+//input: Hola mundo
+//Output: oauo//
+
+
+document.write("<br><br>**ejercicio 9<br>");
+
+let cadena = prompt("ejercicio 9/Escribir texto");
+
+let cantCaracteres = parseInt(cadena.length);
+let cont = 0;
+
+document.write("<br>Vocales del texto ingresado: <br>");
+while(cont<cantCaracteres){
+    if(cadena.charAt(cont)=="a" ||
+    cadena.charAt(cont)=="e" ||
+    cadena.charAt(cont)=="i" || 
+    cadena.charAt(cont)=="o" || 
+    cadena.charAt(cont)=="u"){
+        document.write(cadena.charAt(cont));
+    }
+    cont++
+}*/
--- js/ejercicios.js
+++ js/ejercicios.js
@@ -11,15 +11,32 @@ let comision = "c74i";
 
 //Mostrar mensaje //
 console.log ( 7 + 8);
-console.log(comision);
-console.log("comision" + comision);
+console.log( comision);
+console.log( "comision" +" " + comision);
+
+//muestra el memsaje en el documento html//
+ document.write ("<h1 class= 'tituloPrincipal'> Hola world <br> soy Batman</h1>");
+
+ //ventanas emergentes//
+ alert ("esto es un alerta");//
+
+ //solicitar un color al usuario//
+ let color = prompt ("Ingrese su color favorito");
+
+  console.log(color);
+  //modificamos el contenido de la variable//
+  color = "Rojo";
+  console.log(color);
+
+  let numeroSeleccionado = prompt ("Ingrese un numero entre 1 y 50");
+  console.log (numeroSeleccionado);
 
 
 //TP NUM 1 : Ejercicio 1: Variables y Operaciones Matemáticas //
 let number1 = 4;
 let numer2 = 2;
 
-console.log( number1 + numer2);
+console.log( "El resultado de la suma es" + " " + ( number1 + numer2));
 
 console.log(number1 - numer2);
 
